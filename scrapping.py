715ab09d6795845684f839202cfaef2417dcbde4
Earl Ng <ng.earl@gmail.com>
2021-02-16 21:26:32 +0800

    Added a second loop to look for h2 headers
    Simply duplicated the for loop that went through h1 headers to also look through h2. This increases the computational load of the script. But it increases the perceived accuracy.

--- scrapping.py
+++ scrapping.py
@@ -77,18 +77,73 @@ for filename in os.listdir(directory):
                     signal = 0
                     #print(citation_ref)
                 #focus on heading
-                if "impact" in str(child.text).lower() and (child.tag == "h1" or child.tag == "h2"):
+                if "impact" in str(child.text).lower() and child.tag == "h1":
                     #print("It has a Broader Impact!")
                     #log the title of the broader impact statement
                     impact_statement_title = child.text
                     signal=1
-                elif str(child.text).lower() == "broader impact" and (child.tag == "h1" or child.tag == "h2"):
+                elif str(child.text).lower() == "broader impact" and child.tag == "h1":
                     impact_statement_title = child.text
                     signal=1
-                elif str(child.text).lower() == "broader impacts" and (child.tag == "h1" or child.tag == "h2"):
+                elif str(child.text).lower() == "broader impacts" and child.tag == "h1":
                     impact_statement_title = child.text
                     signal=1
                 #insert a new for loop here to check for the "smaller" parts. The "h2" headers
+                for smaller in child:
+                    if signal == 1 :
+                    #print(section.text)
+                    #broader_dict[filename] = section.text
+                    #loop through any xrefs to count for citations
+                        for xref in smaller:
+                            #narrow down xref citations to bibliography references
+                            if xref.tag == "xref" and xref.attrib['ref-type'] == "bibr":
+                                #use "rid" as the identifier, so we come out of this with a list of references
+                                citation_ref.append(xref.attrib['rid'])
+                                citations +=1
+                        #itertext will make sure that if there are any tags within the section, we still get the whole thing.
+                        impact_statement_text=''.join(smaller.itertext())
+                        impact_statement_number_of_words=len(impact_statement_text.split())
+                        #add count for setences using delimeters of ".", "?", and "!"
+                        impact_statement_number_of_sentences=len(re.split("\.|\?|!", impact_statement_text))-1
+                        #will identify the hash based off of this pattern "86d7c8a08b4aaa1bc7c599473f5dddda-Paper.pdfx.xml"
+                        paper_identifier = re.search("(\w*)(-Paper)", filename)
+                        #check if "positive" is in the statement
+                        has_positive = "True" if "positive" in impact_statement_text.lower() else "False"
+                        #check if "negative" is in the statement
+                        has_negative = "True" if "negative" in impact_statement_text.lower() else "False"
+                        #check if it has the NeurIPS opt-out phrase
+                        has_opt_out = "True" if "this work does not present any foreseeable societal consequence" in impact_statement_text.lower() else "False"
+                        #check if it has "Not Applicable"
+                        has_NA = "True" if "not applicable" in impact_statement_text.lower() else "False"
+                        has_impact_statement = "True"
+                        #add everything to the dictionary
+                        impact_dict["impact title"].append(impact_statement_title)
+                        impact_dict["impact statement"].append(impact_statement_text)
+                        impact_dict["impact statement word count"].append(impact_statement_number_of_words)
+                        impact_dict["impact statement sentence count"].append(impact_statement_number_of_sentences)
+                        impact_dict["citation count"].append(citations)
+                        impact_dict["title"].append(title)
+                        impact_dict["paper identifier"].append(paper_identifier[1])
+                        impact_dict["paper link"].append("https://proceedings.neurips.cc/paper/2020/file/" + paper_identifier[1] + "-Paper.pdf")
+                        impact_dict["has positive"].append(has_positive)
+                        impact_dict["has negative"].append(has_negative)
+                        impact_dict["has opt out"].append(has_opt_out)
+                        impact_dict["has NA"].append(has_NA)
+                        impact_dict["has impact statement"].append(has_impact_statement)
+                        signal = 0
+                        #print(citation_ref)
+                    #focus on heading
+                    if "impact" in str(smaller.text).lower() and smaller.tag == "h2":
+                        #print("It has a Broader Impact!")
+                        #log the title of the broader impact statement
+                        impact_statement_title = smaller.text
+                        signal=1
+                    elif str(smaller.text).lower() == "broader impact" and smaller.tag == "h2":
+                        impact_statement_title = smaller.text
+                        signal=1
+                    elif str(smaller.text).lower() == "broader impacts" and smaller.tag == "h2":
+                        impact_statement_title = smaller.text
+                        signal=1
             #identify the bibliography
             if section.attrib["class"] == "DoCO:Bibliography":
                 #loop through the bibliography section, but we really only want one part
